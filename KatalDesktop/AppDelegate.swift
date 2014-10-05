ede7c474943b08931706b38623fbd7e7073eae25
Keith Ermel <keith.ermel@me.com>
2014-10-05 10:32:43 -0700

    Initial Commit

---
+++ KatalDesktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KatalDesktop
+//
+//  Created by Keith Ermel on 10/5/14.
+//  Copyright (c) 2014 Keith Ermel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KatalDesktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KatalDesktop
+//
+//  Created by Keith Ermel on 10/5/14.
+//  Copyright (c) 2014 Keith Ermel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
